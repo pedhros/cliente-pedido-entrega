233920a062a3b41ec3c64e6ef08858d6d641c798
Pedroh <pedrooohsss@gmail.com>
2024-07-22 20:31:56 -0300

    inicia projeto

---
+++ src/main/java/com/canalfarias/cliente_pedido_entrega/ClientePedidoEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.canalfarias.cliente_pedido_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePedidoEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePedidoEntregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canalfarias/cliente_pedido_entrega/ClientePedidoEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canalfarias.cliente_pedido_entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePedidoEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
