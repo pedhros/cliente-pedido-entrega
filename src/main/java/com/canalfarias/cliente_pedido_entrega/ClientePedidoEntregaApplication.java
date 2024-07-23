467c325d290fc353f6914cfffb33d9b0ab93760c
Pedroh <pedrooohsss@gmail.com>
2024-07-22 21:01:31 -0300

    implementa api de teste

--- src/main/java/com/canalfarias/cliente_pedido_entrega/ClientePedidoEntregaApplication.java
+++ src/main/java/com/canalfarias/cliente_pedido_entrega/ClientePedidoEntregaApplication.java
@@ -2,10 +2,18 @@ package com.canalfarias.cliente_pedido_entrega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePedidoEntregaApplication {
-
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Pedido Entrega - API Home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePedidoEntregaApplication.class, args);
 	}
